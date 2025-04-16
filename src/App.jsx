f6e69b560085235f2e859e42e094e70b8881f36f
mcamilamp <camimerpa@gmail.com>
2025-04-16 18:41:29 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import { FcTodoList } from "react-icons/fc";
+
+function App() {
+  return (
+    <div>
+      <div>
+        <FcTodoList />
+        <h3>Create your task list</h3>
+      </div>
+    </div>
+  );
+}
+
+export default App;
