f5daa0d7551045cbbf0e7caebbf708825e264c9d
mcamilamp <camimerpa@gmail.com>
2025-04-16 18:54:47 -0500

    creating the components and adding basic styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,18 @@ import { FcTodoList } from "react-icons/fc";
 
 function App() {
   return (
-    <div>
+    <div className="w-full h-screen flex flex-col justify-center items-center">
+      <div className="mb-[3rem] flex flex-col items-center">
+        <FcTodoList className="text-[3rem]" />
+        <h3 className="font-semibold">Create your task list</h3>
+      </div>
       <div>
-        <FcTodoList />
-        <h3>Create your task list</h3>
+        <h2>Remaining Task: 0</h2>
+        <select name="" id="">
+          <option value="All">All</option>
+          <option value="Active">Active</option>
+          <option value="Resolved">Resolved</option>
+        </select>
       </div>
     </div>
   );
